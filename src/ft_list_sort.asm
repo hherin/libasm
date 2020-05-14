1e886f3bcca054797f0815bc5d5517d95a8185d1
Helene Herin <heleneherin@macbook-pro-de-helene.home>
2020-05-14 14:14:02 +0200

    list sort done

--- src/ft_list_sort.asm
+++ src/ft_list_sort.asm
@@ -5,3 +5,38 @@ struc s_list
 endstruc
 
 section .text
+global	_ft_list_sort
+
+_ft_swap:
+				mov		r9, [rdi]
+				mov		r10, [rsi]
+				mov		[rdi], r10
+				mov		[rsi], r9
+				ret
+
+_ft_list_sort:
+				mov		rbx, rdi
+				mov		rcx, rsi
+				mov		rdi, qword [rbx]
+				cmp		qword [rdi], 0
+				je		.ret
+.loop:
+				cmp		qword [rdi + s_list.next], 0
+				je		.ret
+				mov		rsi, qword [rdi + s_list.next]
+				mov		r9, rdi
+				mov		rdi, [rdi]
+				mov		rsi, [rsi]
+				call	rcx
+				cmp		rax, 0
+				jg		.if
+				mov		rdi, qword [r9 + s_list.next]
+				jmp		.loop
+.if:
+				call	_ft_swap
+				mov		rdi, qword [rbx]
+				jmp		.loop
+.ret:
+				mov		rdi, rbx
+				ret
+
